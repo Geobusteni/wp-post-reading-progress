b83ad43a76ca673795cff0077488db7cb85935c8
Alexandru Negoita <alexandru@dekode.no>
2020-11-15 01:04:26 +0200

    Adding files back

---
+++ admin/options.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Creates the admin settings menu and page.
+ *
+ * @package WP Post Reading Progress
+ */
+
+declare( strict_types=1 );
+
+namespace WP_POST_READING_PROGRESS\Options;
+
+defined( 'ABSPATH' ) || exit;
