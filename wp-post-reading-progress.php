1678e547963606f0a67f24e15ea1c78d9a8ceaac
Alexandru Negoita <alexandru@dekode.no>
2020-11-30 19:03:45 +0200

    Bumping version to 1.0.0. Preparing for publishing

--- wp-post-reading-progress.php
+++ wp-post-reading-progress.php
@@ -7,7 +7,7 @@
  * Author URI:      https://kul.site
  * Text Domain:     wp-post-reading-progress
  * Domain Path:     /languages
- * Version:         0.1.1
+ * Version:         1.0.0
  *
  * @package         WP Post Reading Progress
  */
